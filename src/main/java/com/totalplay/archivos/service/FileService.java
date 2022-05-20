2b4af2b2d3beed34be31c000c640e552feda7394
Diego González <jgonzalezg@mcllent.com>
2022-05-20 10:15:24 -0500

    actualización de tamaño archivos 20-05-2022

--- src/main/java/com/totalplay/archivos/service/FileService.java
+++ src/main/java/com/totalplay/archivos/service/FileService.java
@@ -72,20 +72,20 @@ public class FileService {
             }
         }
         public Resource load(String filename) {
-            try {
-                Path file = root.resolve(filename);
-                Resource resource = new UrlResource(file.toUri());
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
 
-                if(resource.exists() || resource.isReadable()){
-                    return resource;
-                }else{
-                    throw new RuntimeException("No se puede leer el archivo ");
-                }
-
-            }catch (MalformedURLException e){
-                throw new RuntimeException("Error: " + e.getMessage());
+            if(resource.exists() || resource.isReadable()){
+                return resource;
+            }else{
+                throw new RuntimeException("No se puede leer el archivo ");
             }
+
+        }catch (MalformedURLException e){
+            throw new RuntimeException("Error: " + e.getMessage());
         }
+    }
 
     
 
